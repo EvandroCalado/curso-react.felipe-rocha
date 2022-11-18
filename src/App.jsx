f491f9c7e021cf5f64d460ce4d1e405c578518e5
Evandro <110628201+EvandroCalado@users.noreply.github.com>
2022-11-18 15:43:21 -0300

    build

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,7 @@ import "./App.css";
 
 const App = () => {
   const [tasks, setTasks] = useState([
-    {
-      id: "1",
-      title: "Estudar Programação",
-      completed: false,
-    },
-    {
-      id: "2",
-      title: "Ler Livros",
-      completed: true,
-    },
+    
   ]);
 
   useEffect(() => {
