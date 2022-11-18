f5487006dd48e5123ace743bfd35a3816ac42430
Evandro <110628201+EvandroCalado@users.noreply.github.com>
2022-11-18 01:30:03 -0300

    aplicação com react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import Header from "./componets/Header";
 import Tasks from "./componets/Tasks";
 import AddTask from "./componets/AddTask";
+import TaskDetails from "./componets/TaskDetails";
 
 import "./App.css";
 
@@ -26,9 +29,8 @@ const App = () => {
       return task;
     });
 
-    setTasks(newTasks)
+    setTasks(newTasks);
   };
-  
 
   const handleTaskAddition = (taskTitle) => {
     const newTasks = [
@@ -43,18 +45,33 @@ const App = () => {
     setTasks(newTasks);
   };
 
-  const handleTaskDelete = () => {
-    
-  }
+  const handleTaskDelete = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+
+    setTasks(newTasks);
+  };
 
   return (
-    <>
+    <Router>
       <div className="container">
-        <AddTask 
-        handleTaskAddition={handleTaskAddition} />
-        <Tasks tasks={tasks} handleTaskClick={handleTaskClick} />
+        <Header />
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <>
+              <AddTask handleTaskAddition={handleTaskAddition} />
+              <Tasks
+                tasks={tasks}
+                handleTaskClick={handleTaskClick}
+                handleTaskDelete={handleTaskDelete}
+              />
+            </>
+          )}
+        />
+        <Route path="/:taskTitle" exact component={TaskDetails} />
       </div>
-    </>
+    </Router>
   );
 };
 
--- src/componets/Task.jsx
+++ src/componets/Task.jsx
@@ -1,7 +1,17 @@
+import { render } from "@testing-library/react";
 import React from "react";
+import { CgClose, CgInfo } from "react-icons/cg";
+import { useHistory } from "react-router-dom";
+
 import "./Task.css";
 
-const Task = ({ task, handleTaskClick }) => {
+const Task = ({ task, handleTaskClick, handleTaskDelete }) => {
+  const history = useHistory();
+
+  const handdleTaskDetailsClick = () => {
+    history.push(`/${task.title}`);
+  };
+
   return (
     <div
       className="task-container"
@@ -11,7 +21,19 @@ const Task = ({ task, handleTaskClick }) => {
         {task.title}
       </div>
       <div className="button-cantainer">
-        <button className="remove-task-button">X</button>
+        <button
+          className="see-task-details-button"
+          onClick={handdleTaskDetailsClick}
+        >
+          <CgInfo cursor="pointer" />
+        </button>
+
+        <button
+          className="remove-task-button"
+          onClick={() => handleTaskDelete(task.id)}
+        >
+          <CgClose cursor="pointer" />
+        </button>
       </div>
     </div>
   );
